165d5c5e390ca5bdfae6bc31b1358132e92f8b66
sayalisalape <44391016+sayalisalape@users.noreply.github.com>
2018-11-15 15:32:32 +0530

    Update Fibonacii.c

--- Fibonacii.c
+++ Fibonacii.c
@@ -6,12 +6,10 @@ int main()
 {
     int i, n, t1 = 0, t2 = 1, nextTerm;
 
-    printf("Enter the number of terms: ");
-    scanf("%d", &n);
 
     printf("Fibonacci Series: ");
 
-    for (i = 1; i <= n; ++i)
+    for (i = 1; i <= 5; ++i)
     {
         printf("%d ", t1);
         nextTerm = t1 + t2;
